96d222552d837fce4fc8192c975d6f4f7f8f6911
Aiza Nadeem <nadeema16@coventry.ac.uk>
2025-12-05 21:18:00 +0000

    all the files are made after routing the project will be complete

---
+++ myprj/server.js
@@ -0,0 +1,37 @@
+const express= require('express');
+const session = require('express-session');
+const path = require('path');
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+const authRoutes = require('./routes/authRoutes');
+const petRoutes = require('./routes/petRoutes');
+const applicationRoutes = require('./routes/applicationRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const uploadsDir = path.join(__dirname, 'public', 'uploads');
+if(!fs.existsSync(uploadsDir)){
+    fs.mkdirSync(uploadsDir, {recursive: true});
+}
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.json(__dirname, 'public')));
+app.use(session({
+    secret:'pet-adoption-shelter-key-change',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 24*60*60*100
+    }
+}));
+app.use('/',authRoutes);
+app.use('/',petRoutes);
+app.use('/',applicationRoutes);
+app.use((err, req, res, next)=> {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+    console.log(`make sure you run "npm run init-db" before 'npm start'`);
+});
